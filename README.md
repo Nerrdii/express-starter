9a12fb9f751d03b482a4ae0269aba207ae084038
Joseph Davis <joseph.davis@hrblock.com>
2019-04-01 09:19:19 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Express Starter
+
+Starter for Express API
+
+## Getting Started
+
+### Install dependencies
+
+```bash
+> npm install
+```
+
+### Setup environment variables
+
+Create .env file:
+
+```bash
+> touch .env
+```
+
+Copy variables from `.env.example` to `.env` and replace values, e.g. `MONGO_URI=<MONGO_URI_HERE>`
+
+### Run server
+
+```bash
+> npm run dev
+```
---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }, () =>
+  console.log('Connected to MongoDB')
+);
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(logger('dev'));
+
+app.use('/api/items', require('./routes/api/items'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
